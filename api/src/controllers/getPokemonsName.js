691e98165b8668942a4a7672f0ab50effbb6d1e6
FlorAccastelli <f.accastelli@gmail.com>
2024-02-18 12:12:07 -0300

    getPokeName

--- api/src/controllers/getPokemonsName.js
+++ api/src/controllers/getPokemonsName.js
@@ -3,25 +3,33 @@ const URL = 'https://pokeapi.co/api/v2/pokemon/';
 const { Pokemon } = require('../db');
 
 async function getPokemonByName(req, res) {
-    // try {
+    try {
         const pokemonName = req.query.name.toLowerCase();
         let pokemons = await Pokemon.findAll({ where: { name: pokemonName } });
 
         if(!pokemons || pokemons.length === 0) {
             const response = await axios.get(`${URL}${pokemonName}`);
+            
             const pokemonData = response.data;
-
-            if (pokemonData) {
-                return res.status(200).json(pokemonData);
-            } else {
-                return res.status(404).send("No existe un pokemon con ese nombre");
-            }
-
+            const relevantData = {
+                name: pokemonData.name,
+                image: pokemonData.sprites.front_default,
+                health: pokemonData.stats[0].base_stat,
+                attack: pokemonData.stats[1].base_stat,                    defense: pokemonData.stats[2].base_stat,
+                speed: pokemonData.stats[5].base_stat,
+                height: pokemonData.height,
+                weight: pokemonData.weight
+            };
+            return res.status(200).json(relevantData);
         }
         res.status(200).json(pokemons);
-    // } catch(error) {
-    //     res.status(404).send("No existe un pokemon con ese nombre. SOY EL ULTIMO CATCH")
-    // }
+    } catch(error) {
+        if(error.response && error.response.status === 404){
+            return res.status(404).send("No existe un pokemon con ese nombre");
+        }else{
+            res.status(500).send("Error interno del servidor")
+        }
+    }
 }
 
 module.exports = getPokemonByName;
